b5bb809c94e5cefd461dc434f5cfa97a6e07b9ff
muratcankargi <muratcankargi@hotmail.com>
2022-11-26 20:18:33 +0300

    Created Odev5

---
+++ SQL/Odev5.sql
@@ -0,0 +1,19 @@
+--1. soru
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5
+
+--2. soru
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length 
+OFFSET 5
+LIMIT 5
+
+--3. soru
+SELECT * FROM customer
+WHERE store_id=1
+ORDER BY last_name DESC
+LIMIT 4
+
